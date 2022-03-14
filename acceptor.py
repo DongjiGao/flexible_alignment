97dff6acb5e7f7efc0ef85015a149e0224989604
Dongji Gao <dgao@b14.clsp.jhu.edu>
2022-03-14 09:16:36 -0400

    switched to new topoloy to allow correct speaker id

--- acceptor.py
+++ acceptor.py
@@ -1,57 +1,38 @@
 # 2022 Dongji Gao
 
+import sys
 from collections import defaultdict
 from pathlib import Path
 
 from k2 import SymbolTable
 
 
+# the base Acceptor
 class Acceptor:
-    def __init__(self, input_text, session2spk, output_dir):
-        try:
-            ses2spk = defaultdict(list)
-            with open(session2spk, 'r') as session2spk:
-                for line in session2spk.readlines():
-                    line_list = line.split()
-                    assert len(line_list) == 2
+    def __init__(self, text_file, ses2spk_file):
+        self.text_file = text_file
+        self.ses2spk_file = ses2spk_file
 
-                    session, speaker = line_list
-                    ses2spk[session].append(speaker)
-            self.sesssion2spk = ses2spk
-        except ValueError:
-            print(f"Can not open file {session2spk}.")
-
-        if isinstance(input_text, str):
-            try:
-                texts = {}
-                with open(input_text, 'r') as tf:
-                    for line in tf.readlines():
-                        line_list = line.split()
-                        if len(line_list) >= 2:
-                            speaker = line_list[0]
-                            texts[speaker] = line_list[1:]
-                self.texts = texts
-            except ValueError:
-                print(f"Can not open file {input_text}.")
-        else:
-            self.texts = input_text
-
+        self.symbol_table = ''
         self.unk_token = "<UNK>"
         self.epsilon_token = "<eps>"
-        self.boundary_token = ""
-        self.output_dir = Path(output_dir)
+        self.disambig_token = "#0"
 
-    def set_multi_speaker(self):
-        pass
+    @staticmethod
+    def process_file(text_file, ses2spk_file, symbol_table, unk_id):
+        raise NotImplementedError
+
+    def run(self):
+        raise NotImplementedError
 
     def set_unk(self, unk_token):
-        self.unk_token = unk_word
-
-    def set_boundary(self, boundary_token):
-        self.boundary_token = boundary_token
+        self.unk_token = unk_token
 
     def set_epsilon(self, eps_token):
         self.epsilon_token = eps_token
+
+    def set_disambig(self, disambig_token):
+        self.disambig_token = disambig_token
 
     def set_symbol_table(self, symbol_table):
         self.symbol_table = symbol_table
@@ -62,161 +43,199 @@ class Acceptor:
         except ValueError:
             print(f"Can not get symbol table form file {words}")
 
-    def text_to_symbol(self, text, unk_token, symbol_table):
-        assert len(text) >= 1
-        symbol_text = [symbol_table.get(word) if word in symbol_table else symbol_table.get(
-            unk_token) for word in text]
-        return symbol_text
+    @staticmethod
+    def token_to_id(text, symbol_table, unk_id):
+        assert len(text) > 0
+        text_id = [symbol_table.get(word) if word in symbol_table else unk_id for word in text]
+        return text_id
 
-    def get_arc(self, from_state, to_state, ilabel, olabel, weight):
+    @staticmethod
+    def get_arc(from_state, to_state, ilabel, olabel, weight):
         return f"{from_state}\t{to_state}\t{ilabel}\t{olabel}\t{weight}"
 
-    def build_one(self, text_symbol, epsilon_symbol, unk_symbol, boundary_symbol,
-                  weight, deletion_weight, output_handle):
+
+class FlexibleAcceptor(Acceptor):
+    def __init__(self, text_file, ses2spk_file, output_dir):
+        super().__init__(text_file, ses2spk_file)
+        self.output_dir = Path(output_dir)
+
+    @staticmethod
+    def process_file(text_file, ses2spk_file, symbol_table, unk_id):
+        ses2spk = defaultdict(list)
+        spk2ses = {}
+        try:
+            with open(ses2spk_file, 'r') as s2s:
+                for line in s2s.readlines():
+                    session, speaker = line.split()
+                    ses2spk[session].append(speaker)
+                    spk2ses[speaker] = session
+        except ValueError:
+            print(f"Can not open file {ses2spk_file}")
+
+        spk2utt = defaultdict(list)
+        utt2text = {}
+        ses2order = defaultdict(list)
+        spk_id = defaultdict(int)
+        try:
+            with open(text_file, 'r') as tf:
+                for line in tf.readlines():
+                    line_list = line.split()
+                    if len(line_list) >= 2:
+                        speaker = line_list[0]
+                        text = line_list[1:]
+                        text_id = Acceptor.token_to_id(text, symbol_table, unk_id)
+
+                        id = spk_id[speaker]
+                        spk_id[speaker] += 1
+                        utt = f"{speaker}_{id}"
+
+                        spk2utt[speaker].append(utt)
+                        utt2text[utt] = text_id
+
+                        session = spk2ses[speaker]
+                        ses2order[session].append(utt)
+        except ValueError:
+            print(f"Can not open file {ses2spk_file}")
+
+        return ses2spk, spk2utt, utt2text, ses2order
+
+
+    def build_single(self, utts, utt2text, epsilon_id, disambig_id, weight,
+                     deletion_weight, output_handle):
+        # final_id is for arcs to the final state in k2 FST
+        utt2state = {}
+        arcs = []
 
         # Acceptor Topology
         start_state = 0
         final_state = 1
         next_state = 2
-        cur_state = start_state
 
-        arcs = []
-        for symbol in text_symbol[:-1]:
-            if symbol != boundary_symbol:
-                arc = self.get_arc(cur_state, next_state, symbol, symbol, weight)
+        auxiliary_arc = self.get_arc(start_state, next_state, disambig_id, epsilon_id, weight)
+        arcs.append(auxiliary_arc)
+        cur_state = next_state
+        next_state += 1
+        prev_skip_state = cur_state
+
+        for utt in utts:
+            text_id = utt2text[utt]
+            utt_from_state = cur_state
+
+            for token_id in text_id:
+                arc = self.get_arc(cur_state, next_state, token_id, token_id, weight)
                 arcs.append(arc)
                 cur_state = next_state
                 next_state += 1
-            else:
-                arc = self.get_arc(start_state, cur_state, boundary_symbol, epsilon_symbol,
-                                   deletion_weight)
-                arcs.append(arc)
+            utt_to_state = cur_state
+            utt2state[utt] = (utt_from_state, utt_to_state)
 
-                arc = self.get_arc(cur_state, final_state, boundary_symbol, epsilon_symbol,
-                                   deletion_weight)
-                arcs.append(arc)
+            arc = self.get_arc(prev_skip_state, cur_state, disambig_id, epsilon_id, deletion_weight)
+            arcs.append(arc)
+            prev_skip_state = cur_state
 
-        # last symbol
-        symbol = text_symbol[-1]
-        arc = self.get_arc(cur_state, final_state, symbol, symbol, weight)
-        arcs.append(arc)
-
-        # final state
+        auxiliary_arc = self.get_arc(cur_state, final_state, disambig_id, epsilon_id, weight)
+        arcs.append(auxiliary_arc)
         final_arc = f"{final_state}\t{0}"
 
         for arc in arcs:
             output_handle.write(arc + "\n")
         output_handle.write(final_arc + "\n\n")
 
-    def build_one_multi(self, text_symbol_list, epsilon_symbol, unk_symbol, boundary_symbol,
-                        weight, deletion_weight, output_handle):
+    def build_multi(self, session, ses2spk, spk2utt, utt2text, ses2order,
+                    epsilon_id, disambig_id, weight, deletion_weight, output_handle):
 
-        # start with the longest text
-        max_id = -1
-        max_boundary_counts = -1
-        for id, text_symbol in enumerate(text_symbol_list):
-            boundary_counts = text_symbol.count(boundary_symbol)
-            if boundary_counts > max_boundary_counts:
-                max_id = id
-                max_boundary_counts = boundary_counts
+        utt2state = {}
+        arcs = []
 
         # Acceptor Topology
         start_state = 0
         final_state = 1
         next_state = 2
-        cur_state = start_state
 
-        arcs = []
-        skip_states = []
-        text_symbol = text_symbol_list[max_id]
+        # build for each single speaker
+        for speaker in ses2spk[session]:
+            utts = spk2utt[speaker]
 
-        for symbol in text_symbol[:-1]:
-            if symbol != boundary_symbol:
-                arc = self.get_arc(cur_state, next_state, symbol, symbol, weight)
-                arcs.append(arc)
-                cur_state = next_state
-                next_state += 1
-            else:
-                arc = self.get_arc(start_state, cur_state, boundary_symbol, epsilon_symbol,
+            auxiliary_arc = self.get_arc(start_state, next_state, disambig_id, epsilon_id, weight)
+            arcs.append(auxiliary_arc)
+            cur_state = next_state
+            next_state += 1
+            prev_skip_state = cur_state
+
+            for utt in utts:
+                text_id = utt2text[utt]
+                utt_from_state = cur_state
+
+                for token_id in text_id:
+                    arc = self.get_arc(cur_state, next_state, token_id, token_id, weight)
+                    arcs.append(arc)
+                    cur_state = next_state
+                    next_state += 1
+                utt_to_state = cur_state
+                utt2state[utt] = (utt_from_state, utt_to_state)
+
+                arc = self.get_arc(prev_skip_state, cur_state, disambig_id, epsilon_id,
                                    deletion_weight)
                 arcs.append(arc)
+                prev_skip_state = cur_state
 
-                arc = self.get_arc(cur_state, final_state, boundary_symbol, epsilon_symbol,
-                                   deletion_weight)
-                arcs.append(arc)
-                skip_states.append(cur_state)
+            auxiliary_arc = self.get_arc(cur_state, final_state, disambig_id, epsilon_id, weight)
+            arcs.append(auxiliary_arc)
 
-        # last symbol
-        symbol = text_symbol[-1]
-        arc = self.get_arc(cur_state, final_state, symbol, symbol, weight)
-        arcs.append(arc)
+        # build bypass between speakers
+        ordered_utts = ses2order[session]
+        for index, utt in enumerate(ordered_utts[:-1]):
+            next_utt = ordered_utts[index + 1]
+            from_state = utt2state[utt][1]
+            to_state = utt2state[next_utt][0]
+            if from_state != to_state:
+                bypasa_arc = self.get_arc(from_state, to_state, 11, epsilon_id,
+                                          deletion_weight)
+                arcs.append(bypasa_arc)
 
-        # reuse the skip states
-        # TODO: it's a bit complicated here, write more explanation
-        cur_state = start_state
-        for text_id, text_symbol in enumerate(text_symbol_list):
-            # skip the longest text since we have already built it
-            if text_id != max_id:
-                skip_state_id = 0
-                for symbol_id, symbol in enumerate(text_symbol[:-1]):
-                    if symbol != boundary_symbol:
-                        next_symbol = text_symbol[symbol_id + 1]
-                        if next_symbol != boundary_symbol:
-                            arc = self.get_arc(cur_state, next_state, symbol, symbol, weight)
-                            arcs.append(arc)
-                            cur_state = next_state
-                            next_state += 1
-                        else:
-                            skip_state = skip_states[skip_state_id]
-                            arc = self.get_arc(cur_state, skip_state, symbol, symbol,
-                                               deletion_weight)
-                            arcs.append(arc)
-                            skip_state_id += 1
-                    else:
-                        cur_state = skip_state
-
-                symbol = text_symbol[-1]
-                arc = self.get_arc(cur_state, final_state, symbol, symbol, weight)
-                arcs.append(arc)
-
-        # final state
         final_arc = f"{final_state}\t{0}"
-
         for arc in arcs:
             output_handle.write(arc + "\n")
         output_handle.write(final_arc + "\n\n")
 
-    def build(self, texts, ses2spk, epsilon_token, unk_token, symbol_table, output_dir,
-              boundary_token,
-              weight=0, deletion_weight=0, allow_deletion=True):
-
-        epsilon_symbol = symbol_table.get(epsilon_token)
-        unk_symbol = symbol_table.get(unk_token)
-
-        if allow_deletion:
-            assert boundary_token != ""
-            try:
-                boundary_symbol = symbol_table.get(boundary_token)
-            except ValueError:
-                print(f"Boundary '{boundary_token}' not in symbol table.")
-
+    def build(self, ses2spk, spk2utt, utt2text, ses2order, unk_id, epsilon_id, disambig_id,
+              output_dir, weight=0, deletion_weight=0):
+        spk2accid = {}
         with open(output_dir / "G.fst.txt", 'w') as G:
-            for session in ses2spk:
+            for index, session in enumerate(ses2spk):
                 if len(ses2spk[session]) == 1:
                     speaker = ses2spk[session][0]
-                    text = texts[speaker]
-                    text_symbol = self.text_to_symbol(text, unk_token, symbol_table)
-                    self.build_one(text_symbol, epsilon_symbol, unk_symbol, boundary_symbol,
-                                   weight, deletion_weight, G)
+                    utts = spk2utt[speaker]
+                    spk2accid[speaker] = index
+
+                    self.build_single(utts, utt2text, epsilon_id, disambig_id, weight,
+                                      deletion_weight, G)
                 else:
                     text_symbol_list = []
                     for speaker in ses2spk[session]:
-                        text = texts[speaker]
-                        text_symbol = self.text_to_symbol(text, unk_token, symbol_table)
-                        text_symbol_list.append(text_symbol)
-                    self.build_one_multi(text_symbol_list, epsilon_symbol, unk_symbol,
-                                         boundary_symbol, weight, deletion_weight, G)
+                        spk2accid[speaker] = index
+
+                    self.build_multi(session, ses2spk, spk2utt, utt2text, ses2order,
+                                     epsilon_id, disambig_id, weight, deletion_weight, G)
+
+        return spk2accid
 
     def run(self):
-        self.build(self.texts, self.sesssion2spk, self.epsilon_token, self.unk_token,
-                   self.symbol_table, self.output_dir, self.boundary_token)
+        if self.symbol_table:
+            symbol_table = self.symbol_table
+        else:
+            print("symbol_table not assigned")
+            sys.exit(1)
+
+        unk_id = symbol_table.get(self.unk_token)
+        epsilon_id = symbol_table.get(self.epsilon_token)
+        disambig_id = symbol_table.get(self.disambig_token)
+
+        ses2spk, spk2utt, utt2text, ses2order = self.process_file(self.text_file,
+                                                                  self.ses2spk_file,
+                                                                  symbol_table,
+                                                                  unk_id)
+        output_dir = self.output_dir
+        spk2accid = self.build(ses2spk, spk2utt, utt2text, ses2order, unk_id, epsilon_id,
+                               disambig_id, output_dir)
+        return spk2accid
--- aligner/aligner.py
+++ aligner/aligner.py
@@ -8,7 +8,7 @@ import k2
 import numpy as np
 import torch
 import torch.nn.functional as F
-from acceptor import Acceptor
+from acceptor import FlexibleAcceptor
 from kaldialign import edit_distance
 from snowfall.common import find_first_disambig_symbol
 from snowfall.common import get_texts
@@ -24,21 +24,22 @@ from transformers import (
 
 
 class Aligner():
-    def __init__(self, model, dataset, lang_dir, graph_dir):
+    def __init__(self, model, dataset, lang_dir, graph_dir, output_dir):
         self.model = model
         self.dataset = datasets.load_from_disk(dataset)
         self.lang_dir = Path(lang_dir)
         self.graph_dir = Path(graph_dir)
+        self.output_dir = Path(output_dir)
 
-    def make_g_fst(self):
+    def make_g_fst(self, text, ses2spk, symbol_table, graph_dir):
         raise NotImplementedError
 
-    def make_graph(self):
+    def make_graph(self, lang_dir, graph_dir):
         raise NotImplementedError
 
-    def load_model(self, model):
-        print(f"loading model {model}")
-        self.model = Wav2Vec2ForCTC.from_pretrained(model)
+    def load_model(self, model_file):
+        print(f"loading model {model_file}")
+        model = Wav2Vec2ForCTC.from_pretrained(model_file)
         tokenizer = Wav2Vec2CTCTokenizer("./vocab.json",
                                          unk_token="<UNK>",
                                          pad_token="<eps>",
@@ -48,57 +49,39 @@ class Aligner():
                                                      padding_value=0.0,
                                                      do_normalize=True,
                                                      return_attention_mask=False)
-        self.processor = Wav2Vec2Processor(feature_extractor=feature_extractor,
-                                           tokenizer=tokenizer)
+        processor = Wav2Vec2Processor(feature_extractor=feature_extractor,
+                                      tokenizer=tokenizer)
+        return processor, model
 
     def load_graph(self, graph_dir):
         d = torch.load(graph_dir / "HLG.pt")
-        self.HLG = k2.Fsa.from_dict(d)
+        HLG = k2.Fsa.from_dict(d)
+        return HLG
 
     def decode(self):
         raise NotImplementedError
 
     def get_symbol_table(self, lang_dir):
-        self.symbol_table = k2.SymbolTable.from_file(lang_dir / "words.txt")
+        symbol_table = k2.SymbolTable.from_file(lang_dir / "words.txt")
+        return symbol_table
 
     def run(self):
         raise NotImplementedError
 
 
 class FlexibleAligner(Aligner):
-    def __init__(self, model, dataset, lang_dir, graph_dir):
-        super().__init__(model, dataset, lang_dir, graph_dir)
-        pass
+    def __init__(self, model, dataset, text, ses2spk, lang_dir, graph_dir, output_dir):
+        super().__init__(model, dataset, lang_dir, graph_dir, output_dir)
+        self.text = text
+        self.ses2spk = ses2spk
 
-    def process_text(self, dataset):
-        utt_2_spk = dict()
-        spk_2_text = dict()
-        spk_2_index = dict()
-        id = 0
-        for index in range(len(dataset)):
-            sample = dataset[index]
-            utt_id = sample["utt_id"]
-            spk_id = sample["spk_id"]
-
-            text = sample["text"]
-            if spk_id not in spk_2_text:
-                spk_2_text[spk_id] = text
-                spk_2_index[spk_id] = id
-                id += 1
-            utt_2_spk[utt_id] = spk_id
-        return spk_2_text, utt_2_spk, spk_2_index
-
-    def get_ses2spk(self, ses2spk_file):
-        self.ses2spk_file = ses2spk_file
-
-    def make_g_fst(self, spk_2_text, ses2spk, symbol_table, graph_dir, weight=0, skip_weight=0,
-                   allow_deletion=True):
-        self.acceptor = Acceptor(spk_2_text, ses2spk, graph_dir)
-        self.acceptor.set_symbol_table(symbol_table)
-        self.acceptor.set_boundary("@@")
-        self.acceptor.run()
+    def make_g_fst(self, text, ses2spk, symbol_table, graph_dir):
+        f_acceptor = FlexibleAcceptor(text, ses2spk, graph_dir)
+        f_acceptor.set_symbol_table(symbol_table)
+        spk2accid = f_acceptor.run()
 
         print("Finish making G")
+        return spk2accid
 
     def make_graph(self, lang_dir, graph_dir):
         G_list = list()
@@ -138,8 +121,7 @@ class FlexibleAligner(Aligner):
         print("Finish making graph")
 
     def decode(self, dataset, HLGs, model, processor, symbol_table, graph_dir,
-               utt_2_spk, spk_2_index,
-               search_beam=30.0, output_beam=15.0,
+               spk2index, search_beam=30.0, output_beam=15.0,
                min_active_states=7000, max_active_states=28000, blank_bias=0.0):
 
         hyps_all = list()
@@ -148,9 +130,10 @@ class FlexibleAligner(Aligner):
         for sample in dataset:
 
             utt_id = sample["utt_id"]
-            print(f"decoding {utt_id}")
             spk_id = sample["spk_id"]
-            HLG_index = torch.tensor([spk_2_index[spk_id]], dtype=torch.int32)
+            print(f"Decoding utt: {utt_id} from speaker: {spk_id}.")
+
+            HLG_index = torch.tensor([spk2index[spk_id]], dtype=torch.int32)
             HLG = k2.index_fsa(HLGs, HLG_index)
 
             if not hasattr(HLG, "lm_scores"):
@@ -193,29 +176,34 @@ class FlexibleAligner(Aligner):
         print("done scoring")
         print(errors)
 
+    def write(self, hyp_list, output_dir):
+        with open(output_dir / "text", "w") as output_file:
+            for hyp in hyp_list:
+                if len(hyp) > 1:
+                    output_text = " ".join(hyp)
+                    output_file.write(f"{output_text}\n")
+
     def run(self):
         dataset = self.dataset
-        model = self.model
+        model_file = self.model
         graph_dir = self.graph_dir
         lang_dir = self.lang_dir
+        text_file = self.text
+        ses2spk_file = self.ses2spk
+        output_dir = self.output_dir
 
-        self.load_model(model)
-        spk_2_text, utt_2_spk, spk_2_index = self.process_text(dataset)
-        self.get_symbol_table(lang_dir)
-        self.make_g_fst(spk_2_text, self.ses2spk_file, self.symbol_table, graph_dir)
+        processor, model = self.load_model(model_file)
+        symbol_table = self.get_symbol_table(lang_dir)
+
+        spk2accid = self.make_g_fst(text_file, ses2spk_file, symbol_table, graph_dir)
         self.make_graph(lang_dir, graph_dir)
-        self.load_graph(graph_dir)
+        HLG = self.load_graph(graph_dir)
 
-        HLG = self.HLG
-        processor = self.processor
         hyps_list, refs_list = self.decode(dataset,
                                            HLG,
-                                           self.model,
+                                           model,
                                            processor,
-                                           self.symbol_table,
+                                           symbol_table,
                                            graph_dir,
-                                           utt_2_spk,
-                                           spk_2_index)
-        hyp_text = []
-        for hyp in hyps_list:
-            print(hyp)
+                                           spk2accid)
+        self.write(hyps_list, output_dir)
